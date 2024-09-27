cec4fa4f67dd21c1a92600663f36600c2fd5dc3b
Ryan @ ArchDesktop <ryan@archdesktop>
2024-09-27 12:23:00 -0500

    ignore .class files.

--- WarehouseStage1/Ryan/Waitlist.java
+++ WarehouseStage1/Ryan/Waitlist.java
@@ -1,3 +1,18 @@
-public class Waitlist {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Waitlist {
+    private Map<Client, Product> waitlist;
+
+    public Waitlist() {
+        waitlist = new HashMap<>();
+    }
+
+    public void addProduct(Client client, Product product) {
+        waitlist.put(client, product);
+    }
+
+    public Map<Client, Product> getWaitlist() {
+        return waitlist;
+    }
 }
--- WarehouseStage1/Ryan/Warehouse.java
+++ WarehouseStage1/Ryan/Warehouse.java
@@ -1,111 +1,86 @@
-import java.util.*;
-import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Warehouse implements Serializable {
-    private static final long serialVersionUID = 1L;
-    public static final int PRODUCT_NOT_FOUND = 1;
-    public static final int PRODUCT_NOT_ISSUED = 2;
-    public static final int PRODUCT_HAS_HOLD = 3;
-    public static final int PRODUCT_ISSUED = 4;
-    public static final int HOLD_PLACED = 5;
-    public static final int NO_HOLD_FOUND = 6;
-    public static final int OPERATION_COMPLETED = 7;
-    public static final int OPERATION_FAILED = 8;
-    public static final int NO_SUCH_CLIENT = 9;
+public class Warehouse {
+    private ClientList clients;
+    private ProductList products;
+    private List<Transaction> transactions;
 
-    private ProductList productList;
-    private ClientList clientList;
-    private static Warehouse warehouse;
-
-    private Warehouse() {
-        productList = ProductList.instance();
-        clientList = ClientList.instance();
+    public Warehouse() {
+        clients = new ClientList();
+        products = new ProductList();
+        transactions = new ArrayList<>();
     }
 
-    public static Warehouse instance() {
-        if (warehouse == null) {
-            ClientIdServer.instance(); // instantiate all singletons
-            return (warehouse = new Warehouse());
-        } else {
-            return warehouse;
-        }
+    // Add a new client to the system
+    public void addClient(Client client) {
+        clients.addClient(client);
     }
 
+<<<<<<< HEAD
     public void addProduct(String name, String id, int stock) {
         Product product = new Product(id, name, 0.0, stock);
         productList.addProduct(product);
+=======
+    // Add a new product to the system using ProductList
+    public void addProduct(Product product) {
+        products.addProduct(product);
+>>>>>>> 357d302 (ignore .class files.)
     }
 
-    public Client addClient(String name, String address, String phone) {
-        Client client = new Client(name, address, phone);
-        if (clientList.insertClient(client)) {
-            return client;
+    // Find a product by ID using ProductList
+    public Product findProduct(String productId) {
+        return products.findProduct(productId);
+    }
+
+<<<<<<< HEAD
+    public Iterator<Product> getProducts() {
+        return productList.getProducts().iterator();
+=======
+    // Add a product to a client's wishlist (now using Product's own wishlist)
+    public void addToWishlist(Client client, Product product) {
+        product.addToWishlist(client); // Use Product's method to add client to its wishlist
+>>>>>>> 357d302 (ignore .class files.)
+    }
+
+    // Add a product to a client's waitlist (now using Product's own waitlist)
+    public void addToWaitlist(Client client, Product product) {
+        product.addToWaitlist(client); // Use Product's method to add client to its waitlist
+    }
+
+    // Process an order for a client
+    public void processOrder(Client client, String productId, int quantity) {
+        Product product = findProduct(productId); // Find product using ProductList
+        if (product != null) {
+            if (product.stockLevel() >= quantity) {
+                Transaction transaction = new Transaction(client, product, quantity);
+                transactions.add(transaction); // Add transaction to the list
+                product.setStockLevel(product.stockLevel() - quantity); // Decrease stock level
+            } else {
+                addToWaitlist(client, product); // Add to waitlist if insufficient stock
+            }
+        } else {
+            System.out.println("Product not found: " + productId);
+        }
+    }
+
+    // Query the status of a transaction
+    public Transaction getTransactionStatus(int transactionId) {
+        for (Transaction transaction : transactions) {
+            if (transaction.getId() == transactionId) {
+                return transaction;
+            }
         }
         return null;
     }
 
-    public Iterator<Product> getProducts() {
-        return productList.getProducts().iterator();
+    // A method to retrieve all products
+    public List<Product> getAllProducts() {
+        return products.getProducts();
     }
 
-    public Iterator<Client> getClients() {
-        return clientList.getClients();
-    }
-
-    public static Warehouse retrieve() {
-        try {
-            FileInputStream file = new FileInputStream("WarehouseData");
-            ObjectInputStream input = new ObjectInputStream(file);
-            input.readObject();
-            ClientIdServer.retrieve(input);
-            return warehouse;
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-            return null;
-        } catch (ClassNotFoundException cnfe) {
-            cnfe.printStackTrace();
-            return null;
-        }
-    }
-
-    public static boolean save() {
-        try {
-            FileOutputStream file = new FileOutputStream("WarehouseData");
-            ObjectOutputStream output = new ObjectOutputStream(file);
-            output.writeObject(warehouse);
-            output.writeObject(ClientIdServer.instance());
-            return true;
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-            return false;
-        }
-    }
-
-    private void writeObject(ObjectOutputStream output) {
-        try {
-            output.defaultWriteObject();
-            output.writeObject(warehouse);
-        } catch (IOException ioe) {
-            System.out.println(ioe);
-        }
-    }
-
-    private void readObject(ObjectInputStream input) {
-        try {
-            input.defaultReadObject();
-            if (warehouse == null) {
-                warehouse = (Warehouse) input.readObject();
-            } else {
-                input.readObject();
-            }
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public String toString() {
-        return productList + "\n" + clientList;
+    // A method to retrieve all clients
+    public List<Client> getAllClients() {
+        return clients.getClients();
     }
 }
