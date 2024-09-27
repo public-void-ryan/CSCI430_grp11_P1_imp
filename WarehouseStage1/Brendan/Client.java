1494c46bbb1732124011a367d15ad42260a0e089
brendankariniemi <bkarin22@icloud.com>
2024-09-27 16:05:34 -0500

    Push intial Client/ClientList code.

--- WarehouseStage1/Brendan/Client.java
+++ WarehouseStage1/Brendan/Client.java
@@ -1,3 +1,31 @@
-public class Client {
+import java.io.Serializable;
 
+public class Client implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String name;
+    private String id;
+    private static final String CLIENT_STRING = "C";
+    private static int idCounter = 1;
+
+    public Client(String name) {
+        this.name = name;
+        this.id = CLIENT_STRING + idCounter++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public boolean equals(String id) {
+        return this.id.equals(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Client [Name=" + name + ", ID=" + id + "]";
+    }
 }
--- WarehouseStage1/Brendan/ClientList.java
+++ WarehouseStage1/Brendan/ClientList.java
@@ -1,3 +1,46 @@
-public class ClientList {
+import java.io.Serializable;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
+public class ClientList implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private List<Client> clients = new LinkedList<>();
+    private static ClientList clientList;
+
+    private ClientList() {
+    }
+
+    public static ClientList instance() {
+        if (clientList == null) {
+            clientList = new ClientList();
+        }
+        return clientList;
+    }
+
+    public boolean addClient(Client client) {
+        return clients.add(client);
+    }
+
+    public Iterator<Client> getClients() {
+        return clients.iterator();
+    }
+
+    public Client search(String clientId) {
+        for (Client client : clients) {
+            if (client.getId().equals(clientId)) {
+                return client;
+            }
+        }
+        return null;
+    }
+
+    private Object readResolve() {
+        return instance();
+    }
+
+    @Override
+    public String toString() {
+        return clients.toString();
+    }
 }
