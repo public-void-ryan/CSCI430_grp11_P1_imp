6aae16a12fc7c75e64558edd096e8a4dfc13a96c
Ryan @ ArchDesktop <ryan@archdesktop>
2024-10-25 16:51:36 -0500

    print client id with wishlist

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -184,7 +184,7 @@ public class UserInterface {
     public void showClientWishlist() {
         String clientId = getToken("Enter client ID: ");
         Iterator<Wishlist.WishlistItem> wishlist = warehouse.getClientWishlistItems(clientId);
-        System.out.println("Client's Wishlist:");
+        System.out.println("Client " + clientId + " Wishlist:");
         while (wishlist.hasNext()) {
             Wishlist.WishlistItem item = wishlist.next();
             System.out.println(item);
@@ -193,12 +193,13 @@ public class UserInterface {
 
     public void showClientTransactions() {
         String clientId = getToken("Enter client ID: ");
-        //Iterator<Transaction> transactions = warehouse.getClientTransactions(clientId);
-        //System.out.println("Client's Transactions:");
-        //while (transactions.hasNext()) {
-        //    Transaction transaction = transactions.next();
-        //    System.out.println(transaction);
-        //}
+        // Iterator<Transaction> transactions =
+        // warehouse.getClientTransactions(clientId);
+        // System.out.println("Client's Transactions:");
+        // while (transactions.hasNext()) {
+        // Transaction transaction = transactions.next();
+        // System.out.println(transaction);
+        // }
     }
 
     public void showProduct() {
